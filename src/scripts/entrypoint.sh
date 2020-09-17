a32b7335b8e787349fd553e6f2b921aa8356d9ea
Fabio D'Andria <fabio.dandria@laserromae.it>
2020-09-17 13:58:49 +0200

    update startup script

--- src/scripts/entrypoint.sh
+++ src/scripts/entrypoint.sh
@@ -5,7 +5,7 @@ cd /opt/liferay/tomcat-9.0.10/bin
 # changing the amount of RAM to be used for Java to 75% of available mem
 memory=$( echo "($( free -m | grep '^Mem:' | tr -s ' ' | cut -d ' ' -f 2 ) / 1024 * 0.75 * 1024)/1; scale=0" | bc )
 echo "setting tomcat to ${memory} mb"
-sed -i "s#2560#${memory}#g" /opt/liferay/tomcat-9.0.10/bin/setenv.sh
+sed -i "s#2560#${memory}#g" /opt/liferay/tomcat-9.0.17/bin/setenv.sh
 
 echo "starting tomcat"
 sh startup.sh
