a4e8ed9c945afb2e5347134819e64711a7f89a68
Fabio D'Andria <fabio.dandria@laserromae.it>
2020-10-21 17:55:19 +0200

    add elastic cluster info

--- src/scripts/entrypoint.sh
+++ src/scripts/entrypoint.sh
@@ -10,6 +10,15 @@ cat <<EOF > /opt/liferay/tomcat-9.0.17/bin/setenv.sh
 CATALINA_OPTS="$CATALINA_OPTS -Dfile.encoding=UTF8 -Djava.net.preferIPv4Stack=true -Dorg.apache.catalina.loader.WebappClassLoader.ENABLE_CLEAR_REFERENCES=false -Duser.timezone=GMT -Xms${memory}m -Xmx${memory}m -XX:MaxNewSize=1536m -XX:MaxMetaspaceSize=384m -XX:MetaspaceSize=384m -XX:NewSize=1536m -XX:SurvivorRatio=7 -XX:MaxPermSize=2048m"
 JAVA_HOME="/usr/lib/jvm/java-1.8.0-openjdk"
 EOF
+
+cat <<EOF > /opt/liferay/osgi/configs/com.liferay.portal.search.elasticsearch6.configuration.ElasticsearchConfiguration.cfg
+operantionMode="REMOTE"
+transportAdresses="liferay-es.processo-standard-ril.svc.cluster.local:9300"
+logExceptionsOnly="false"
+clusterName="LiferayElasticsearchCluster"
+EOF
+
+
 echo "starting tomcat"
 sh startup.sh
 
